52d15a5ea387a53b16a1a17a51fdaa57a56d9d2e
Emiliano Villalpando <ewc0mx1@gmail.com>
2021-07-06 21:31:55 -0500

    infitnessv1.2

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const path = require('path')
 const app = express()
 
 //MIDDLEWARE
-app.set('view engine', 'ejs')
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
