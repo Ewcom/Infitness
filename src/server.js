fd1c2a120aad97e21ba8bdf7ffca48949bf1cec8
Emiliano Villalpando <ewc0mx1@gmail.com>
2021-07-16 23:08:56 -0500

    infitness 2.0 ui updates

--- src/server.js
+++ src/server.js
@@ -11,25 +11,25 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.sendFile('/html/index.html', { root: './public' });
 
 
 })
 
-app.get('/sanAndres',(req,res)=>{
+app.get('/sanAndres', (req, res) => {
     res.sendFile('/html/sanAndres.html', { root: './public' });
 
 
 })
 
-app.get('/sanPedro',(req,res)=>{
+app.get('/sanPedro', (req, res) => {
     res.sendFile('/html/sanPedro.html', { root: './public' });
 
 
 })
 
-app.get('/laCalera',(req,res)=>{
+app.get('/laCalera', (req, res) => {
     res.sendFile('/html/calera.html', { root: './public' });
 
 
@@ -40,6 +40,6 @@ app.get('/laCalera',(req,res)=>{
 
 
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 3000, function () {
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-  });
+});
